685312168970fc83eed752ad927739d241762087
saul <sauld@cimat.mx>
2023-02-15 17:25:33 -0700

    with new strucutre

---
+++ IntermediateR/test_dev.R
@@ -0,0 +1,13 @@
+f <- function(a) g(a)
+g <- function(b) h(b)
+h <- function(c) i(c)
+i <- function(d) {
+    if (!is.numeric(d)) {
+        stop("`d` must be numeric", call. = FALSE)
+    }
+    d + 10
+}
+
+main <- function() {
+    f("a")
+}
